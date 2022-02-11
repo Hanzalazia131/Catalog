2071e6f00c99f4f13760dc43f38fe87a8f346515
Hanzala-FA18-bse-037 <FA18-BSE-037@cuilahore.edu.pk>
2022-02-11 12:14:07 +0500

    Mongo complete

---
+++ Repositories/MongoDbItemsRepository.cs
@@ -0,0 +1,54 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Catalog.Entities;
+using MongoDB.Bson;
+using MongoDB.Driver;
+
+namespace Catalog.Repositories
+{
+    public class MongoDbItemsRepository : IItemsRepository
+    {
+        private const string databaseName = "Catalog";
+        private const string collectionName = "Items";
+        private readonly IMongoCollection<Item> itemsCollection;
+        private readonly FilterDefinitionBuilder<Item> filterBuilder = Builders<Item>.Filter;
+        public MongoDbItemsRepository(IMongoClient mongoClient)
+        {
+            IMongoDatabase database = mongoClient.GetDatabase(databaseName);
+            itemsCollection = database.GetCollection<Item>(collectionName);
+        }
+
+        public void CreateItem(Item item)
+        {
+            itemsCollection.InsertOne(item);
+        }
+
+        public void DeleteItem(Guid Id)
+        {
+            var deleteFilter = filterBuilder.Eq(item => item.Id, Id);
+            itemsCollection.DeleteOne(deleteFilter);
+            // var item = 
+            // itemsCollection.DeleteOne(item);
+        }
+
+        public Item GetItem(Guid id)
+        {
+            var filter = filterBuilder.Eq(item => item.Id, id);
+            return itemsCollection.Find(filter).SingleOrDefault();
+        }
+
+        public IEnumerable<Item> GetItems()
+        {
+            // return itemsCollection.Find(new BsonDocument()).ToList();
+            return itemsCollection.AsQueryable();
+        }
+
+        public void UpdateItem(Item item)
+        {
+            var filter = filterBuilder.Eq(existingItem => existingItem.Id, item.Id);
+            itemsCollection.ReplaceOne(filter, item);
+        }
+    }
+}
